24f019ee316a36f5bb12094f8a49b45fe4f6122c
syeichkhalil30@gmail.com <syeichkhalil@gmail.com>
2024-05-10 05:48:21 +0700

    feat: create reports table and relate to users table

---
+++ database/migrations/2024_05_09_181510_create_reports_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable(false);
+            $table->string('title', 50)->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->string('location', 100)->nullable(false);
+            $table->enum('status', ['diterima', 'ditolak', 'sedang diproses', 'selesai'])->default('diterima')->nullable(false);
+            $table->dateTime('time')->nullable(false);
+            $table->string('unic_code', 6)->nullable(false);
+            $table->boolean('is_private')->default(0)->nullable(false);
+            $table->boolean('is_anonymous')->default(0)->nullable(false);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
