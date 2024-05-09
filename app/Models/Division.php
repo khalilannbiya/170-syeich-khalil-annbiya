e8634304545e19e21b67064c5593c1c143ff2364
Syeich Khalil Annbiya <84386261+khalilannbiya@users.noreply.github.com>
2024-05-10 06:35:31 +0700

    feat: create report_divisions table and relate to reports and divisions tables (#9)
    Co-authored-by: syeichkhalil30@gmail.com <syeichkhalil@gmail.com>

--- app/Models/Division.php
+++ app/Models/Division.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\ReportDivision;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Division extends Model
 {
@@ -19,5 +20,10 @@ class Division extends Model
     public function users(): HasMany
     {
         return $this->hasMany(User::class);
+    }
+
+    public function reportDivisions(): HasMany
+    {
+        return $this->hasMany(ReportDivision::class);
     }
 }
--- app/Models/Report.php
+++ app/Models/Report.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\ReportDivision;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Report extends Model
 {
@@ -26,5 +28,10 @@ class Report extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function reportDivisions(): HasMany
+    {
+        return $this->hasMany(ReportDivision::class);
     }
 }
