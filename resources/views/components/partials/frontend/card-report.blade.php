9943ed88112fc8f462161da482d5b6b2bbf786b9
syeichkhalil30@gmail.com <syeichkhalil@gmail.com>
2024-05-28 07:38:22 +0700

    feat: create report history

--- resources/views/components/partials/frontend/card-report.blade.php
+++ resources/views/components/partials/frontend/card-report.blade.php
@@ -1,32 +1,33 @@
 <article
-    class="group flex w-full sm:max-w-lg md:max-w-[22rem] lg:max-w-md xl:max-w-sm h-auto flex-col rounded-md bg-white bg-clip-border text-gray-700 shadow-md hover:shadow-2xl transition-all duration-700 ease-in-out">
-    <div class="p-6">
-        <h1 id="report-title"
-            class="mb-2 block text-xl font-semibold leading-snug tracking-normal text-black antialiased">
-            Pembacokan di daerah wadas uhuy slebwe
-        </h1>
-        <p class="mb-2 text-sm font-semibold tracking-normal text-gray-600 capitalize antialiased">
-            syeich khalil annbiya
-        </p>
-        <p class="mb-2 text-sm font-semibold tracking-normal text-gray-400 antialiased">
-            20:00, 20-04-2024
-        </p>
-        <p class="mb-2 text-sm capitalize font-semibold tracking-normal text-gray-400 antialiased">
-            Pemerkosaan dan perbuatan cabul
-        </p>
-        <p id="report-description"
-            class="block text-base text-primary font-normal leading-relaxed text-inherit antialiased">
-            Garis kejut di depan PKU Gamping menurut saya kurang nyaman dan bahaya, karena berada di
-            jalur cepat. Banyak yang tiba-tiba
-        </p>
-    </div>
-    <div class="p-6 pt-0 flex justify-between items-center">
-        <p class="capitalize text-sm font-semibold">sedang diproses
-        </p>
-        <a href="#" aria-label="Lihat Detail Laporan"
-            class="flex items-center justify-between px-4 gap-3 select-none rounded-md bg-black py-3 text-center align-middle text-sm font-bold text-white shadow-md shadow-slate-500/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-            Detail
-        </a>
+    {{ $attributes->merge(['class' => 'group flex w-full h-[20rem] md:h-[25rem] lg:h-[20rem] sm:max-w-lg md:max-w-[22rem] lg:max-w-md xl:max-w-sm  flex-col rounded-md bg-white bg-clip-border text-gray-700 shadow-md hover:shadow-2xl transition-all duration-700 ease-in-out']) }}>
+    <div class="flex flex-col justify-between h-full">
+        <div class="p-6">
+            <h1 id="report-title"
+                class="block mb-2 text-xl antialiased font-semibold leading-snug tracking-normal text-black">
+                {{ $report->title }}
+            </h1>
+            <p class="mb-2 text-sm antialiased font-semibold tracking-normal text-gray-600 capitalize">
+                {{ $report->name }}
+            </p>
+            <p class="mb-2 text-sm antialiased font-semibold tracking-normal text-gray-400">
+                {{ $report->created_at->format('H:i, d-m-Y') }}
+            </p>
+            <p class="mb-2 text-sm antialiased font-semibold tracking-normal text-gray-400 capitalize">
+                {{ $report->name }}
+            </p>
+            <p id="report-description"
+                class="block text-base antialiased font-normal leading-relaxed text-primary text-inherit">
+                {{ $report->description }}
+            </p>
+        </div>
+        <div class="flex items-center justify-between p-6 pt-0">
+            <p class="text-sm font-semibold capitalize">{{ $report->status }}
+            </p>
+            <a href="#" aria-label="Lihat Detail Laporan"
+                class="flex items-center justify-between px-4 gap-3 select-none rounded-md bg-black py-3 text-center align-middle text-sm font-bold text-white shadow-md shadow-slate-500/20 transition-all hover:shadow-lg hover:shadow-slate-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                Detail
+            </a>
+        </div>
     </div>
 </article>
 
